db87aad47809d3e857e6faa170b33b365dddd1f9
Janus2585 <bd.yhome@gmail.com>
2017-08-31 14:59:56 -0500

    Made accessToken, refreshToken, profile info visible using console.log

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 //get express.js framework
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys.js");
+
 //app is used to set up configuration to route request to different handlers
 const app = express();
-//Tutorial code 
+//Tutorial code
 //create route handler and associate it with a given route
-//app: Express App to register this route handler 
+//app: Express App to register this route handler
 //get: watch for incoming requests with this method
 //'/': watch for requests trying to access '/'. / indicates it is trying to access the root route (localhost:5000/). If this was '/greeting' instead, then it can be accessed by going to localhost:5000/greeting
 //req: object representing the incoming request
@@ -18,12 +20,37 @@ app.get('/', (req, res) => {
 });
 */
 
-passport.use(new GoogleStrategy());
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: "/auth/google/callback"
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log('access token', accessToken);
+			console.log('refresh token', refreshToken);
+			console.log('profile:', profile);
+		}
+	)
+);
+
+//adding a single route handler for google OAuth
+app.get(
+	"/auth/google",
+	passport.authenticate("google", {
+		//requesting the user's profile and email address from google
+		scope: ["profile", "email"]
+	})
+);
+
+app.get(
+	"/auth/google/callback", 
+	passport.authenticate('google'));
+
 //dynamically determine which port to listen to to make it compatible with Heroku
 //If there is an env variable assigned by Heroku, then assign that to PORT.
 //If env is not defined (for example in a development environment), then assign it 5000;
 const PORT = process.env.PORT || 5000;
 //listen on PORT
 app.listen(PORT);
-
-
