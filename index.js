cf6501aa733634864335f9ec8149a22b373ae4be
Janus2585 <bd.yhome@gmail.com>
2017-08-30 23:11:39 -0500

    started implementing Google OAuth2.0 into index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 //get express.js framework
 const express = require('express');
-
-//node js does not have support for ES 2015 modules. This is why import express from 
-//'express' is not used and "require is used instead"
-
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 //app is used to set up configuration to route request to different handlers
 const app = express();
-
+//Tutorial code 
 //create route handler and associate it with a given route
 //app: Express App to register this route handler 
 //get: watch for incoming requests with this method
@@ -14,10 +12,12 @@ const app = express();
 //req: object representing the incoming request
 //res: object representing the outgoing response
 //res.send({hi: 'there'}): immediately send some JSON back to whatever made this request
+/* 
 app.get('/', (req, res) => {
 	res.send({ subsequentDeployTest: 'success'});
 });
-
+*/
+passport.use(new GoogleStrategy());
 //dynamically determine which port to listen to to make it compatible with Heroku
 //If there is an env variable assigned by Heroku, then assign that to PORT.
 //If env is not defined (for example in a development environment), then assign it 5000;
