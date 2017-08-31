c9a8e91d7503ec9d3e1c762bde90808d766e30f9
Janus2585 <bd.yhome@gmail.com>
2017-08-30 19:17:49 -0500

    I learned how to deploy this app to Heroku. Dynamic port binding. Specified node environment in package.json. Specified startup script. Created a .gitignore file to exclude the node_modules folder. node_modules does not need to be included because Heroku will do that for us.

--- index.js
+++ index.js
@@ -18,5 +18,11 @@ app.get('/', (req, res) => {
 	res.send({ hi: 'there'});
 });
 
-//listen on localhost:5000
-app.listen(5000);
+//dynamically determine which port to listen to to make it compatible with Heroku
+//If there is an env variable assigned by Heroku, then assign that to PORT.
+//If env is not defined (for example in a development environment), then assign it 5000;
+const PORT = process.env.PORT || 5000;
+//listen on PORT
+app.listen(PORT);
+
+
