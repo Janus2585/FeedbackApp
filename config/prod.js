de4198f0c2f0c8809031d5f1df6a648cdffa4c1d
Janus2585 <bd.yhome@gmail.com>
2017-09-03 06:18:58 -0500

    Finished authorization flow. Created a production set of credentials. keys.js now only contains logic to determine if in dev or prod mode.

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+//do commit - production credentials
+module.exports = {
+	//environment variables are all caps with underscores, by convention
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey process.env.COOKIE_KEY: 
+};
