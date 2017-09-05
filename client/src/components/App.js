9b2e175a3220110de79900d5038563b4d5d817ea
Janus2585 <bd.yhome@gmail.com>
2017-09-05 16:03:56 -0500

    React Router is functional. Basic Components display at the correct routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,10 +12,17 @@ const App = () => {
 	return (
 		//place browser routes, each specifies a relation between a possible address the user can visit, and a component to display
 		//BrowserRouter can only have 1 child at most
+		//exact means the path must be exactly the string in path="" to show the component
+		//By default, the component will be shown if the string in path="" is included in the path
+		//Header must always be visible
 		<div>
 			<BrowserRouter>
 				<div>
-					<Route path="/" component={Landing} />
+					<Header />
+					<Route exact path="/" component={Landing} />
+					<Route exact path= "/surveys" component={Dashboard} />
+					<Route path="/surveys/new" component={SurveyNew} />
+					
 				</div>
 			</BrowserRouter>
 		</div>
