b3fec65dde2093f9a26d29316ac9542e8f55641a
Janus2585 <bd.yhome@gmail.com>
2017-09-05 15:39:52 -0500

    Route path landing component test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+//dummy components for testing
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+// end of dummy components
 
 const App = () => {
 	return (
+		//place browser routes, each specifies a relation between a possible address the user can visit, and a component to display
+		//BrowserRouter can only have 1 child at most
 		<div>
-			Hi There
+			<BrowserRouter>
+				<div>
+					<Route path="/" component={Landing} />
+				</div>
+			</BrowserRouter>
 		</div>
 	);
 };
