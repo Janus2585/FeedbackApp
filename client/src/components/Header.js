1f430171b034dca7de6f8c87668ed9845681d24e
Janus2585 <bd.yhome@gmail.com>
2017-09-06 00:44:58 -0500

    header shows correct text depending on if the user is signed in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,37 @@
 //
 
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 class Header extends Component {
+	renderContent() {
+		switch(this.props.auth) {
+			//login status pending
+			case null:
+				return;
+			//not logged in
+			case false: 
+				return <li><a href="/auth/google">Login With Google</a></li>;
+					
+			//logged in
+			default: 
+				return <li><a>Logout</a></li>;
+		}
+	}
+
 	render() {
 		return(
 			<nav>
 			    <div className="nav-wrapper">
 			      <a href="#" className="left brand-logo">Feedback App</a>
 			      <ul className="right">
-			        <li><a href="sass.html">Login With Google</a></li>
+			        {this.renderContent()}
 			      </ul>
 			    </div>
 			</nav>
 		);
 	}
 }
-
-export default Header;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+export default connect(mapStateToProps) (Header);
