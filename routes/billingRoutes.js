2e184c5600e30bac279ebbb9026ca647b25cd1fe
Janus2585 <bd.yhome@gmail.com>
2017-09-07 12:46:01 -0500

    finished request handler that charges the user, updates the number of credits they have, and sends the user model back to what made the request. billingRoutes.js checks if the user is logged in before proceeding

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,13 +4,16 @@ const stripe = require('stripe')(keys.stripeSecretKey
 
 
 module.exports = app => {
-	app.post('/api/stripe', (req, res) => {
+	app.post('/api/stripe', async (req, res) => {
 		//handle token, reach out to Stripe API
-		stripe.charges.create({
+		const charge = await stripe.charges.create({
 			amount: 500,
 			currency: 'usd',
 			description:'5 dollars for 5 credits',
-			source: req.body.id
+			source: req.body.id //obtained from stripe.js
 		});
+		req.user.credits += 5;	//access the current user to add credits to their account
+		const user = await req.user.save();//save the change. this is an asynch request. assign user to req.user to ensure the lastest version of user is used in the future
+		res.send(user)//send the updated user info back to the browser
 	});
 };
