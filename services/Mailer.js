90b0f1eb39f11d5f0809624c451b00e47c129c42
Janus2585 <bd.yhome@gmail.com>
2017-09-08 17:56:00 -0500

    boiler plate for sending emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,13 +3,30 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
-	constructor({ subject, recipients }, content) {
+	constructor({ subject, recipients }, content) { //take the list of recipients
 		super();
 
 		this.from_email = new helper.Email('no-reply@feedbackapp.com');
 		this.subject = subject;
 		this.body = new helper.Content('text/html', content);
-		this.recipients = this.formatAddresses(recipients);
-
+		this.recipients = this.formatAddresses(recipients); //pass list of recipients to formatAddresses
+	
+		this.addContent(this.body);//register this.body with the Mailer
+		this.addClickTracking();//enable click tracking
+		this.addRecipients();
 	}
-}
+
+	formatAddresses(recipients) {
+		return recipients.map(({ email }) => { //For every recipient in the array we pull out the email property. extra set of () around email is necessary for destructuring with an arrow function. 
+			return new helper.Email(email); //format the email with helper
+		});
+	}
+
+	addClickTracking() {
+		const trackingSettings = new helper.TrackingSettings();
+		const clickTracking = new helper.ClickTracking(true, true);
+
+		trackingSettings.setClickTracking(clickTracking);
+		this.addTrackingSettings(trackingSettings);
+	}
+}	
