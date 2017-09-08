2ac3ef0c612bbbfafb30a8860de7cbf58515688c
Janus2585 <bd.yhome@gmail.com>
2017-09-08 17:35:45 -0500

    build Mailer class properties

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
 const Survey = mongoose.model('surveys');
 
@@ -17,5 +19,9 @@ module.exports = app => {
 			_user: req.user.id,//req.user.id is available on any mongoose model
 			dateSent: Date.now() //assumes the survey is sent now
 		});
+
+	//Send email here
+	const mailer = new Mailer(survey, surveyTemplate(survey));
+
 	});
 };
