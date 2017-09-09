a4d761ba9d81437b624fc380acea216b6f1e57ea
Janus2585 <bd.yhome@gmail.com>
2017-09-08 23:09:50 -0500

    setup sendgrid API request

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,14 +6,15 @@ class Mailer extends helper.Mail {
 	constructor({ subject, recipients }, content) { //take the list of recipients
 		super();
 
+		this.sgAPI = sendgrid(keys.sendGridKey);
 		this.from_email = new helper.Email('no-reply@feedbackapp.com');
 		this.subject = subject;
 		this.body = new helper.Content('text/html', content);
 		this.recipients = this.formatAddresses(recipients); //pass list of recipients to formatAddresses
 	
-		this.addContent(this.body);//register this.body with the Mailer
-		this.addClickTracking();//enable click tracking
-		this.addRecipients();
+		this.addContent(this.body);//register this.body with the Mailer. this function is defined by the base class
+		this.addClickTracking();//enable click tracking. this function is defined below
+		this.addRecipients();//process the list of recipients. this function is defined below
 	}
 
 	formatAddresses(recipients) {
@@ -28,5 +29,27 @@ class Mailer extends helper.Mail {
 
 		trackingSettings.setClickTracking(clickTracking);
 		this.addTrackingSettings(trackingSettings);
+	}
+
+	addRecipients() {
+		const personalize = new helper.Personalization();
+		//iterate over the list of recipients in this.recipients, and for each make use of personalize
+		this.recipients.forEach(recipient => { 
+			personalize.addTo(recipient); //add each recipient to the personalize object
+		});
+		this.addPersonalization(personalize)//add the entire personalize object to this
+		
+	}
+
+	async send() {
+		//create sendgrid API request
+		const request = this.sgAPI.emptyRequest({
+			method: 'POST',
+			path: '/v3/mail/send',
+			body: this.toJSON()
+		});
+
+		const reponse = this.sgAPI.API(request);//send the request
+		return response;
 	}
 }	
